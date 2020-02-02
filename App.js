05a6972e74b014dd1702afc10aeef9dc1132b7b4
JoakimNagornoff <Joakim.nagornoff@live.se>
2020-02-02 16:44:08 +0100

    first page done

--- App.js
+++ App.js
@@ -10,34 +10,70 @@ import React from 'react';
 
 import {StyleSheet, Text, View, TouchableOpacity, Button} from 'react-native';
 
-
-export default class App extends React.Component{
-  render(){
-    return (
-      <View style={style.container}>
-        <Text style={style.title}>CityPop</Text>
-  
-
-
-    
-      </View>
-  
-    );
-  }
+const setScreen = (screen) => {
+  setState({screen: screen})
 }
 
-const style = StyleSheet.create({
-  container:{
-    flex: 1,
-    alignItems: "center",
-  },
+export class App extends React.Component{
+  render(){
+      return (
+        <View style={style.container}>
+          <View style={style.half1}>
+            <Text style={style.title}>CityPop</Text>
 
-  title:{
-    fontFamily: 'Cochin',
-    fontSize: 35,
-    fontWeight: 'bold',
-    marginTop: 100,
+
+
+          </View>
+          <View style={style.half2}>
+            <TouchableOpacity style={style.Button}>
+              <Text style={style.buttonText}>SEARCH BY CITY</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={style.Button}>
+              <Text style={style.buttonText}>SEARCH BY COUNTRY</Text>
+            </TouchableOpacity>
+
+
+          </View>
+        </View>
+     
+      )
+  }
+}
+const style = StyleSheet.create({
+ container: {
+   flex: 1,
+
+  },
+  half1: {
+    flex: 1,
   
+  },
+  half2: {
+    flex: 2,
+  
+  
+  },
+  title:{
+    fontSize: 35,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 100,
+
+  },
+  Button: {
+    alignItems: "center",
+    backgroundColor: '#DDDDDD',
+    width: 420,
+    height: 80,
+    marginBottom: 10,
+
+  },
+  buttonText: {
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 20,
   }
 
 });
+export default App;
